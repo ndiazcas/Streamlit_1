3abe16ea79eaa821b4e5598acbaf9108f5da04f6
ndiazcas <79842381+ndiazcas@users.noreply.github.com>
2024-03-29 09:32:12 -0400

    Update

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,6 +15,48 @@ option = st.selectbox(
 )
 st.write('You selected:', option)
 
+if option == 'Furniture':
+   st.bar_chart(df, x=df[df['Category'] == 'Furniture'], y='Sales')
+
+   st.dataframe(df.groupby("Category").sum())
+   st.bar_chart(df.groupby("Category", as_index=False).sum(), x=df[df['Category'] == 'Furniture'], y="Sales", color="#04f")
+   
+   df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+   df.set_index('Order_Date', inplace=True)
+   sales_by_month_furniture = df.filter(items=['Sales', df['Category'] == 'Furniture']).groupby(pd.Grouper(freq='M')).sum()
+   
+   st.dataframe(sales_by_month_furniture)
+   
+   st.line_chart(sales_by_month_furniture, y="Sales")
+
+   elif option == 'Office Supplies':
+      st.bar_chart(df, x=df[df['Category'] == 'Office Supplies'], y='Sales')
+
+      st.dataframe(df.groupby("Category").sum())
+      st.bar_chart(df.groupby("Category", as_index=False).sum(), x=df[df['Category'] == 'Office Supplies'], y="Sales", color="#04f")
+   
+      df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+      df.set_index('Order_Date', inplace=True)
+      sales_by_month_office = df.filter(items=['Sales', df['Category'] == 'Office Supplies']).groupby(pd.Grouper(freq='M')).sum()
+   
+      st.dataframe(sales_by_month_office)
+   
+      st.line_chart(sales_by_month_office, y="Sales")
+
+   else:
+      st.bar_chart(df, x=df[df['Category'] == 'Technology'], y='Sales')
+
+      st.dataframe(df.groupby("Category").sum())
+      st.bar_chart(df.groupby("Category", as_index=False).sum(), x=df[df['Category'] == 'Technology'], y="Sales", color="#04f")
+   
+      df["Order_Date"] = pd.to_datetime(df["Order_Date"])
+      df.set_index('Order_Date', inplace=True)
+      sales_by_month_tech = df.filter(items=['Sales', df['Category'] == 'Technology']).groupby(pd.Grouper(freq='M')).sum()
+   
+      st.dataframe(sales_by_month_tech)
+   
+      st.line_chart(sales_by_month_tech, y="Sales")
+
 # This bar chart will not have solid bars--but lines--because the detail data is being graphed independently
 st.bar_chart(df, x="Category", y="Sales")
 
